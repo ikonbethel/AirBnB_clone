80f8318722473b437af3364aef7b53b369da3d4d
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2023-03-12 23:48:13 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""
-Module Amenity class
-"""
-
+'''This module creates a Amenity class'''
 from models.base_model import BaseModel
 
-class Amenity(BaseModel):
-    """Class for managing amenity objects"""
 
+class Amenity(BaseModel):
+    '''Class for managing amenity objects'''
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        '''Initializes attributes for the Amenity class'''
+        super().__init__(*args, **kwargs)
