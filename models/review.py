bc8a076bca1a3fee8ef8560a7f0a34e07c448c7d
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2023-03-12 22:23:49 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -3,8 +3,11 @@
 Module Review class
 """
 
+from models.base_model import BaseModel
 
-class Review():
+class Review(BaseModel):
+    """Class for managing review objects"""
+
     place_id = ""
     user_id = ""
     text = ""
