0035fe554beefdffd5b8ec28bb6fa548828be5d5
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2023-03-12 22:26:27 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -4,8 +4,9 @@ Module User class
 """
 from models.base_model import BaseModel
 
-
 class User(BaseModel):
+    """Class for managing user objects"""
+    
     email = ""
     password = ""
     first_name = ""
