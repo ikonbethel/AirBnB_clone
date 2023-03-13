9dce2b2754f4e3ca95c7be1ea101ba731b14af0e
MBN <milkabiniam@gmail.com>
2023-03-13 04:03:35 +0300

    l

--- models/user.py
+++ models/user.py
@@ -4,8 +4,11 @@ Module User class
 """
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
-    """Class for managing user objects"""
+    """
+    Class for managing user objects
+    """
     
     email = ""
     password = ""
